ea2b061606cf48be393157f7546f437d9f02c670
Andres Collazos <andres.collazos@viirtue.com>
2024-08-08 09:30:43 -0500

    feat(project-fe): adjust navbar component

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './models/navbar.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  protected menuItems: MenuItem[] = [
+    {
+      class: 'fa-brands fa-whatsapp',
+      url: 'https://wa.me/573103593479'
+    },
+    {
+      class: 'fa-brands fa-instagram',
+      url: 'https://www.instagram.com/adolfojinete'
+    },
+  ];
 
 }
